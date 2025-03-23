e197570eec1f4bdd4738aed181b399648c559a94
ALIMINETI ANIRUDH <146748508+Anirudh9247@users.noreply.github.com>
2025-03-23 11:09:50 +0530

    Create duplicate.cpp

---
+++ duplicate.cpp
@@ -0,0 +1,11 @@
+#include <unordered_set>
+#include <vector>
+
+bool containsDuplicate(std::vector<int>& nums) {
+    std::unordered_set<int> seen;
+    for (int num : nums) {
+        if (seen.count(num)) return true;
+        seen.insert(num);
+    }
+    return false;
+}
